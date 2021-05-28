fd77bcaf31d4bf47a4ecb6646ea2062fab26c0a4
Luke-Mitchell <luke.mitchell@billie.io>
2021-05-28 15:22:13 +0200

    tried some small changes

--- force-app/main/default/lwc/apiCaller/apiCaller.js
+++ force-app/main/default/lwc/apiCaller/apiCaller.js
@@ -1,12 +1,11 @@
 import { LightningElement } from 'lwc';
 import callEndpoint from '@salesforce/apex/ApiCallController.callEndpoint';
 
-
 export default class ApiCaller extends LightningElement {
 
   handleClick(event) {
 
-    let peopleList = callEndpoint('https://enr7thjj1rpu72t.m.pipedream.net');
+    const peopleList = callEndpoint('https://enr7thjj1rpu72t.m.pipedream.net');
 
     console.log(peopleList);
 
