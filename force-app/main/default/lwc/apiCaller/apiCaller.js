40160c489f54157185689eced5fb788f34185c7a
Luke-Mitchell <luke.mitchell@billie.io>
2021-05-28 14:46:26 +0200

    Added ApiCallController class and ApiCaller LWC

---
+++ force-app/main/default/lwc/apiCaller/apiCaller.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+import callEndpoint from '@salesforce/apex/ApiCallController.callEndpoint';
+
+
+export default class ApiCaller extends LightningElement {
+
+  handleClick(event) {
+
+    let peopleList = callEndpoint('https://enr7thjj1rpu72t.m.pipedream.net');
+
+    console.log(peopleList);
+
+  }
+}
